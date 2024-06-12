04e81edd989e991161a4b6274c18257c057b38f8
Benjamin <benjaminsatana@gmail.com>
2024-06-12 15:12:42 -0400

    .

--- app/(main)/(pages)/maintenance/departments/page.tsx
+++ app/(main)/(pages)/maintenance/departments/page.tsx
@@ -3,9 +3,6 @@ import DepartmentTabs from "@/Features/maintenance/Components/DepartmentTabs";
 export default function page() {
     return (
         <div className="card">
-            <div className="card-header">
-                <h3 className="card-title">Departments</h3>
-            </div>
             <DepartmentTabs />
         </div>
     );
