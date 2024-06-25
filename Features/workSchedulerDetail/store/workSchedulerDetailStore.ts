68f91f21fce0f3e4dd91dbe4bb6e9946a415e57f
Benjamin <benjaminsatana@gmail.com>
2024-06-25 08:00:15 -0400

    added impor

--- Features/workSchedulerDetail/store/workSchedulerDetailStore.ts
+++ Features/workSchedulerDetail/store/workSchedulerDetailStore.ts
@@ -1,5 +1,5 @@
-import { IWorkSchedulerDetail } from '@/Features/workScheduler/Types/IWorkScheduler';
-import create from 'zustand';
+import create from "zustand";
+import { IWorkSchedulerDetail } from "../Types/IWorkSchedulerDetail";
 
 interface WorkSchedulerDetailStore {
     data: IWorkSchedulerDetail[];
@@ -9,14 +9,20 @@ interface WorkSchedulerDetailStore {
     clearData: () => void;
 }
 
-export const useWorkSchedulerDetailStore = create<WorkSchedulerDetailStore>((set) => ({
-    data: [] as IWorkSchedulerDetail[],
-    addWorkSchedulerDetail: (data) => set((state) => ({ data: [...state.data, data] })),
-    setData: (data) => set((state) => ({ data: [data] })),
-    removeWorkSchedulerDetail(id) {
-        set((state) => ({
-            data: state.data.filter((workSchedulerDetail) => workSchedulerDetail.idWorkSchedulerDetail !== id),
-        }));
-    },
-    clearData: () => set({ data: [] }),
-}));
+export const useWorkSchedulerDetailStore = create<WorkSchedulerDetailStore>(
+    (set) => ({
+        data: [] as IWorkSchedulerDetail[],
+        addWorkSchedulerDetail: (data) =>
+            set((state) => ({ data: [...state.data, data] })),
+        setData: (data) => set((state) => ({ data: [data] })),
+        removeWorkSchedulerDetail(id) {
+            set((state) => ({
+                data: state.data.filter(
+                    (workSchedulerDetail) =>
+                        workSchedulerDetail.idWorkSchedulerDetail !== id
+                ),
+            }));
+        },
+        clearData: () => set({ data: [] }),
+    })
+);
