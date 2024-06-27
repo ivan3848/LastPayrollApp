7282a066d09ca9e22d1645d5ceb5f8a9f99a68cf
Benjamin <benjaminsatana@gmail.com>
2024-06-27 10:31:14 -0400

    added option to see employee changes to look the option

--- Features/employee/Components/EmployeeChanges.tsx
+++ Features/employee/Components/EmployeeChanges.tsx
@@ -1,19 +1,129 @@
-"use client";
-import { TabPanel, TabView } from "primereact/tabview";
-import { CiBank } from "react-icons/ci";
+"use client"
+import ActionTableTemplate from "@/Features/Shared/Components/ActionTableTemplate";
+import useParamFilter from "@/Features/Shared/Hooks/useParamFilter";
+import { Column } from "primereact/column";
+import {
+    DataTable,
+    DataTablePageEvent,
+    DataTableSortEvent,
+} from "primereact/datatable";
+import useEmployeeQuery from "@/Features/employee/Hooks/useEmployeeQuery";
+import { IWorkScheduler } from "@/Features/workScheduler/Types/IWorkScheduler";
 
-const EmployeeChanges = () => {
-    return (
-        <div className="grid crud-demo">
-            <div className="col-12">
-                <TabView scrollable>
-                    <TabPanel
-                        header="Cambios"
-                        leftIcon={<CiBank className="mr-2" size={18} />}
-                    ></TabPanel>
-                </TabView>
-            </div>
+interface Props {
+    submitted: boolean;
+    handleEdit: (entity: IWorkScheduler) => void;
+    handleDelete: (entity: IWorkScheduler) => void;
+    handleAdd: (entity: IWorkScheduler) => void;
+}
+
+const EmployeeChanges = ({ submitted, handleDelete, handleEdit }: Props) => {
+    const {
+        setPage,
+        setPageSize,
+        setFilters,
+        setSorts,
+        clearSorts,
+        clearFilters,
+        params,
+    } = useParamFilter();
+
+    const listOfDependencies: boolean[] = [submitted];
+    const { data, isLoading } = useEmployeeQuery(params, listOfDependencies);
+
+    const onPage = (event: DataTablePageEvent) => {
+        setPage(event.page! + 1);
+        setPageSize(event.rows);
+    };
+
+    const onSort = (event: DataTableSortEvent) => {
+        switch (event.sortOrder) {
+            case 1:
+                setSorts([{ sortBy: event.sortField, isAsc: true }]);
+                break;
+            case -1:
+                setSorts([{ sortBy: event.sortField, isAsc: false }]);
+                break;
+            default:
+                clearSorts();
+                break;
+        }
+    };
+
+    const onFilter = (event: any) => {
+        setFilters([
+            {
+                column: event.field,
+                value: event.constraints.constraints?.[0].value,
+            },
+        ]);
+    };
+
+    const header = (
+        <div className="flex flex-column md:flex-row md:justify-content-between md:align-items-center">
+            <h3 className="m-0">Horario</h3>
         </div>
+    );
+
+    return (
+        <DataTable
+            id="workScheduler-Table"
+            dataKey="idEmployee"
+            value={data?.items}
+            lazy
+            paginator
+            loading={isLoading}
+            onSort={onSort}
+            removableSort
+            sortField={params.filter?.sorts?.[0]?.sortBy ?? ""}
+            sortOrder={params.filter?.sorts?.[0]?.isAsc ? 1 : -1}
+            sortMode="single"
+            totalRecords={data?.totalCount}
+            className="datatable-responsive"
+            paginatorTemplate="FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink RowsPerPageDropdown"
+            emptyMessage="No hay registros para mostrar."
+            header={header}
+            onPage={onPage}
+            rowsPerPageOptions={[5, 10, 25]}
+            rows={data?.pageSize!}
+            first={data.firstRow!}
+            currentPageReportTemplate="Mostrando registros del {first} al {last} de {totalRecords}"
+        >
+            <Column
+                field="workSchedulerName"
+                header="Horario"
+                filter
+                filterField="name"
+                filterPlaceholder="Buscar por Horario"
+                showFilterMenuOptions={false}
+                onFilterApplyClick={(e) => onFilter(e)}
+                onFilterClear={clearFilters}
+            ></Column>
+            <Column
+                field="employeeName"
+                header="Nombre del empleado"
+                headerStyle={{ minWidth: "15rem" }}
+                sortable
+                filter
+                filterField="name"
+                filterPlaceholder="Buscar por departamento"
+                showFilterMenuOptions={false}
+                onFilterApplyClick={(e) => onFilter(e)}
+                onFilterClear={clearFilters}
+            ></Column>
+
+            <Column
+                header="Acciones"
+                body={(rowData) => (
+                    <ActionTableTemplate<IWorkScheduler>
+                        entity={rowData}
+                        handleDelete={handleDelete}
+                        handleEdit={handleEdit}
+                    />
+                )}
+                headerStyle={{ minWidth: "10rem" }}
+            ></Column>
+        </DataTable>
     );
 };
 
