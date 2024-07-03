efee87f5256cb668cf7020971866da09b5ec8bac
Luis F. Segura <122110623+luisfco0215@users.noreply.github.com>
2024-07-03 17:02:57 -0400

    Woring on FiredEmployee...

--- Features/employee/Components/FireEmployee.tsx
+++ Features/employee/Components/FireEmployee.tsx
@@ -14,12 +14,27 @@ interface Props {
     employee: IEmployee
 }
 
+interface IFireEmployee {
+    IdEmployee: number;
+    FiredDate: Date;
+    Comment: string;
+    IsTakenVacation: boolean;
+    IsUnemployment: boolean;
+    IsPreview: boolean;
+    IsNotice: boolean;
+    IsChristmasPayment: boolean;
+    IdStatusFired: number;
+    IdCancelationType: number;
+}
+
 const FireEmployee = ({ employee }: Props) => {
     const { setFilters, params, setAllData } = useParamFilter();
     const [pagoNavidad, setPagoNavidad] = useState(false);
     const [notice, setNotice] = useState(false);
     const [vacation, setVacation] = useState(false);
     const [unemployment, setUnemployment] = useState(false);
+
+    console.log(employee);
 
     const {
         handleSubmit,
@@ -28,7 +43,7 @@ const FireEmployee = ({ employee }: Props) => {
         setValue,
         watch,
         formState: { errors },
-    } = useForm<IPositionSalary>();
+    } = useForm<IFireEmployee>();
 
     // const changePositionSalaryEdit = useChangePositionSalaryQuery({
     //     toast,
@@ -37,7 +52,8 @@ const FireEmployee = ({ employee }: Props) => {
     //     reset,
     // });
 
-    const onFormSubmit = (data: IPositionSalary) => {
+    const onFormSubmit = (data: IFireEmployee) => {
+        console.log("test", data);
         // changePositionSalaryEdit.mutate(data);
         return;
     }
@@ -55,16 +71,16 @@ const FireEmployee = ({ employee }: Props) => {
                                 </label>
                                 <GenericDropDown
                                     id="idHierarchyPositionManager"
-                                    isValid={!!errors.idHierarchyPositionManager}
+                                    isValid={!!errors.IdStatusFired}
                                     text="name"
                                     useQuery={useEmployeeQuery}
                                     setValue={setValue}
                                     watch={watch}
                                     param={params}
                                 />
-                                {errors.idHierarchyPositionManager && (
+                                {errors.IdStatusFired && (
                                     <small className="p-invalid text-danger">
-                                        {errors.idHierarchyPositionManager.message?.toString()}
+                                        {errors.IdStatusFired.message?.toString()}
                                     </small>
                                 )}
                             </div>
@@ -73,33 +89,40 @@ const FireEmployee = ({ employee }: Props) => {
                                     Motivo
                                 </label>
                                 <GenericStatusDropDown
-                                    id="IdNewStatus"
-                                    isValid={!!errors.IdNewStatus}
+                                    id="IdCancelationType"
+                                    isValid={!!errors.IdCancelationType}
                                     setValue={setValue}
                                     watch={watch}
                                     isFocus={true}
                                     tableName="SalaryNewsStatus"
                                 />
-                                {errors.IdNewStatus && (
+                                {errors.IdCancelationType && (
                                     <small className="p-invalid text-danger">
-                                        {errors.IdNewStatus.message?.toString()}
+                                        {errors.IdCancelationType.message?.toString()}
                                     </small>
                                 )}
                             </div>
                             <div className="field col-12 md:col-3">
                                 <label id='DateChange' htmlFor="DateChange">Fecha de inicio</label>
-                                <Calendar value={new Date()} showIcon />
+                                <Calendar
+                                    {...register("FiredDate")}
+                                    name='FiredDate'
+                                    value={new Date()} showIcon />
                             </div>
                         </div>
                         <div className="p-fluid formgrid grid" style={{ marginTop: '15px', marginBottom: '15px', display: 'flex', justifyContent: "space-evenly", width: "100%" }}>
                             <div className="field col-12 md:col-3">
                                 <h6>Pago de Navidad</h6>
                                 <InputSwitch
+                                    {...register("IsChristmasPayment")}
+                                    name='IsChristmasPayment'
                                     checked={pagoNavidad}
                                     onChange={(e) => setPagoNavidad(e.value ?? false)}
                                 />
                                 <h6>Pre-Aviso</h6>
                                 <InputSwitch
+                                    {...register("IsNotice")}
+                                    name='IsNotice'
                                     checked={notice}
                                     onChange={(e) => setNotice(e.value ?? false)}
                                 />
@@ -107,11 +130,15 @@ const FireEmployee = ({ employee }: Props) => {
                             <div className="field col-12 md:col-3">
                                 <h6>Ha tomado vacaciones este año?</h6>
                                 <InputSwitch
+                                    {...register("IsTakenVacation")}
+                                    name='IsTakenVacation'
                                     checked={vacation}
                                     onChange={(e) => setVacation(e.value ?? false)}
                                 />
                                 <h6>Cesantia</h6>
                                 <InputSwitch
+                                    {...register("IsUnemployment")}
+                                    name='IsUnemployment'
                                     checked={unemployment}
                                     onChange={(e) => setUnemployment(e.value ?? false)}
                                 />
@@ -119,8 +146,8 @@ const FireEmployee = ({ employee }: Props) => {
                             <div className="field col-12 md:col-3">
                                 <label htmlFor="Description">Descripción</label>
                                 <InputTextarea
-                                    {...register("Description")}
-                                    id="Description"
+                                    {...register("Comment")}
+                                    id="Comment"
                                     placeholder='Ingrese descripción...' rows={3} cols={30} />
                             </div>
                         </div>
