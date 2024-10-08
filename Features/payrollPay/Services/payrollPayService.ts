d2301c37184d58056cc92394bae1dab2eec83f63
Luis F. Segura <122110623+luisfco0215@users.noreply.github.com>
2024-10-08 13:23:19 -0400

    Working to fix bugs and add generate receipt by employee.

--- Features/payrollPay/Services/payrollPayService.ts
+++ Features/payrollPay/Services/payrollPayService.ts
@@ -3,13 +3,15 @@ import { IPaymentLoad, IPayrollPay } from "../types/IPayrollPay";
 import { IEmployee } from "@/Features/employee/Types/IEmployee";
 import { IDeletePayrollPayDto } from "../Components/DeletePayrollDialog";
 import { IGetPaymentLoadsDto } from "../Components/GenerateFiles";
+import { IGenerateReceipt } from "../Components/GenerateReceipt";
+import { IGetPayrollExecution } from "../types/IGetPayrollExecution";
 
 const payrollPayService = new ApiService<IPayrollPay, IPayrollPay>("employee/payrollPay");
 const deletePayrollPayService = new ApiService<IDeletePayrollPayDto, IPayrollPay>("employee/payrollPay/delete");
 const employeesForPayrollPayService = new ApiService<number[], IEmployee[]>("employee/payrollPay/byIdEmployees");
 const generatePayrollPayService = new ApiService<IPayrollPay, IPayrollPay>("employee/payrollPay/generatePayroll");
 const generateFilesService = new ApiService<IGetPaymentLoadsDto, IPaymentLoad>("employee/payrollPay/downloadPayrollPaymentTxt");
-
+const generateReceiptService = new ApiService<IGenerateReceipt, IGetPayrollExecution[]>("employee/payrollPay/generateReceipts");
 
 export default payrollPayService;
 
@@ -17,6 +19,7 @@ export {
     generatePayrollPayService,
     employeesForPayrollPayService,
     deletePayrollPayService,
-    generateFilesService
+    generateFilesService,
+    generateReceiptService
 };
 
