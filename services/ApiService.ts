556b56ab77d3f14125e0d558887dab5f84205596
Yeduin <ymateo@objectlink.net>
2024-10-15 10:26:58 -0400

    working with Report

--- services/ApiService.ts
+++ services/ApiService.ts
@@ -10,8 +10,8 @@ import Cookies from "js-cookie";
 const user = Cookies.get("auth") as IUser | undefined;
 
 const axiosInstance = axios.create({
-    //baseURL: "http://localhost:5038/",
-    baseURL: "http://specialistnomgateway.objectlink.com:5038/",
+    baseURL: "http://localhost:5038/",
+    //baseURL: "http://specialistnomgateway.objectlink.com:5038/",
     headers: {
         "Content-Type": "application/json",
         IdCompany: user?.idCompany ?? "2",
