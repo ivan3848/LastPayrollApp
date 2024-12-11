17b3ca5e1136369a2b6254d93a945a91e1c739a8
yeduin <el_yeduin_02@hotmail.com>
2024-12-11 17:17:28 -0400

    fixing commit

--- Features/costCenter/Components/CostCenterTable.tsx
+++ Features/costCenter/Components/CostCenterTable.tsx
@@ -37,7 +37,7 @@ const CostCenterTable = ({
 
     const listOfDependencies: boolean[] = [submitted];
     const { data, isFetching } = useCostCenterQuery(params, listOfDependencies);
-    
+
     const onPage = (event: DataTablePageEvent) => {
         setPage(event.page! + 1);
         setPageSize(event.rows);
@@ -164,79 +164,6 @@ const CostCenterTable = ({
                 headerStyle={{ minWidth: "10rem" }}
             ></Column>
         </DataTable>
-        // <>
-        //     {true ? (
-        //         <div className="border-round border-1 surface-border p-4">
-        //             <div className="flex mb-3">
-        //                 <div>
-        //                     <Skeleton width="25rem" className="mb-2"></Skeleton>
-        //                 </div>
-        //             </div>
-        //             <Skeleton width="100%" height="150px"></Skeleton>
-        //         </div>
-        //     ) : (
-        //         <DataTable
-        //             id="CostCenter-Table"
-        //             dataKey="idCostCenter"
-        //             value={data?.items}
-        //             lazy
-        //             paginator
-        //             loading={isFetching}
-        //             onSort={onSort}
-        //             removableSort
-        //             sortField={params.filter?.sorts?.[0]?.sortBy ?? ""}
-        //             sortOrder={params.filter?.sorts?.[0]?.isAsc ? 1 : -1}
-        //             sortMode="single"
-        //             totalRecords={data?.totalCount}
-        //             className="datatable-responsive"
-        //             paginatorTemplate="FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink RowsPerPageDropdown"
-        //             emptyMessage="No hay registros para mostrar."
-        //             header={header}
-        //             onPage={onPage}
-        //             rowsPerPageOptions={[5, 10, 25]}
-        //             rows={data?.pageSize!}
-        //             first={data.firstRow!}
-        //             currentPageReportTemplate="Mostrando registros del {first} al {last} de {totalRecords}"
-        //         >
-        //             <Column
-        //                 field="description"
-        //                 header="Centro de costo"
-        //                 headerStyle={{ minWidth: "15rem" }}
-        //                 sortable
-        //                 filter
-        //                 filterField="description"
-        //                 filterPlaceholder="Buscar por centro de costo"
-        //                 showFilterMenuOptions={false}
-        //                 onFilterApplyClick={(e) => onFilter(e)}
-        //                 onFilterClear={clearFilters}
-        //             ></Column>
-        //             <Column
-        //                 field="accountNumber"
-        //                 header="Número de cuenta"
-        //                 headerStyle={{ minWidth: "15rem" }}
-        //                 sortable
-        //                 filter
-        //                 filterField="accountNumber"
-        //                 filterPlaceholder="Buscar por número de cuenta"
-        //                 showFilterMenuOptions={false}
-        //                 onFilterApplyClick={(e) => onFilter(e)}
-        //                 onFilterClear={clearFilters}
-        //             ></Column>
-        //             <Column
-        //                 header="Acciones"
-        //                 body={(rowData) => (
-        //                     <ActionTableTemplate<ICostCenter>
-        //                         entity={rowData}
-        //                         handleDelete={handleDelete}
-        //                         handleEdit={handleEdit}
-        //                         accessName="NOMINA"
-        //                     />
-        //                 )}
-        //                 headerStyle={{ minWidth: "10rem" }}
-        //             ></Column>
-        //         </DataTable>
-        //     )}
-        // </>
     );
 };
 
